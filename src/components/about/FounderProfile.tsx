98430a61342ee82b38019d73328d6149ef872302
PUSHP RAJ SHARMA <98100352+pushp314@users.noreply.github.com>
2025-05-13 14:28:21 +0530

    changes about

--- src/components/about/FounderProfile.tsx
+++ src/components/about/FounderProfile.tsx
@@ -3,7 +3,8 @@ const FounderProfile = () => {
     <div className="w-full overflow-hidden">
       <section className="relative bg-[#FFFCF5] pt-1 px-4 sm:px-8 md:px-20 h-auto sm:h-[400px] md:h-[500px]">
         {/* Yellow semicircle - responsive positioning */}
-        <div className="absolute right-[-80px] sm:right-[-60px] md:right-[-80px] bottom-[-100px] sm:bottom-[-80px] md:bottom-[-100px] w-[280px] sm:w-[400px] md:w-[700px] z-0 overflow-hidden">
+        <div className="absolute right-[-100px] sm:right-[-60px] md:right-[-80px] bottom-[-120px] sm:bottom-[-80px] md:bottom-[-100px] w-[390px] sm:w-[400px] md:w-[700px] z-0 overflow-hidden">
+
           <img
             src="https://res.cloudinary.com/dxkwt2ssi/image/upload/v1746640325/aboutbg_wow27t.svg"
             alt="Yellow semicircle"
@@ -12,7 +13,8 @@ const FounderProfile = () => {
         </div>
 
         {/* Content - responsive alignment */}
-        <div className="relative z-10 flex flex-col items-center sm:items-end pr-0 sm:pr-6 md:pr-36 mt-8 sm:mt-0 ">
+        <div className="relative z-10 flex flex-col items-end sm:items-end pl-4 mb-3  sm:pl-0 sm:pr-6 md:pr-36 mt-8 sm:mt-0 ">
+
           {/* Image - responsive sizing */}
           <div className="w-[200px] h-[200px] sm:w-[240px] sm:h-[240px] md:w-[280px] md:h-[280px] rounded-lg overflow-hidden border-4 border-gray-200 shadow-lg bg-white mb-4 transform transition-transform hover:scale-[1.02]">
             <img
